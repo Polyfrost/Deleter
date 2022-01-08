cbcc550a982fb6397239ca617ac47cce9bed275a
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-01-08 21:52:11 +0700

    1.3 add support for multiple files update dependencies

--- src/main/kotlin/Deleter.kt
+++ src/main/kotlin/Deleter.kt
@@ -1,16 +1,13 @@
-import org.jetbrains.annotations.NotNull
 import java.io.File
 import javax.swing.JFrame
 import javax.swing.UIManager
 import javax.swing.WindowConstants
 import kotlin.system.exitProcess
 
-
-var file: File? = null
-var delete = ""
-val frame = JFrame("Deleter")
 var tries = 0
-fun main(@NotNull args : Array<String>) {
+val frame = JFrame("Deleter")
+
+fun main(args : Array<String>) {
 
     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
     frame.setSize(500, 500)
@@ -19,28 +16,27 @@ fun main(@NotNull args : Array<String>) {
     frame.setLocationRelativeTo(null)
     frame.isResizable = false
     frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-    delete = args[0]
-    println(delete)
-    file = File(delete)
-    delete()
+    for (file in args) {
+        tries = 0
+        delete(File(file))
+    }
     Thread.sleep(1000)
     exitProcess(0)
 }
 
-private fun delete() {
-    println(file?.path)
-    if (!file?.exists()!!) {
-        println("the file doesn't exist lol")
+private fun delete(file: File) {
+    if (!file.exists()) {
+        println("That file does not exist!")
         return
     }
-    if (!file!!.delete()) {
+    if (!file.delete()) {
         tries += 1
         if (tries >= 4) {
-            println("ok i give up")
+            println("Too many tries, cancelling.")
             return
         }
-        println("file failed to delete... trying again...")
+        println("File failed to delete, trying again.")
         Thread.sleep((1000 * tries).toLong())
-        delete()
+        delete(file)
     }
 }
